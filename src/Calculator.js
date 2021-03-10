3f9a109837c4429adee3eba12bd259c9c25b6f92
Sushanth Mali <sushanthhere@gmail.com>
2021-03-10 17:34:56 -0500

    Resolving inner expressions code added

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,7 +7,8 @@ class Calculator extends Component {
         super(props);
         this.state = {
             mathStr : '',
-            result : 0
+            result : '',
+            errorMessage : ''
         };
         this.calculate = this.calculate.bind(this);
         this.changeMathStr = this.changeMathStr.bind(this);
@@ -15,10 +16,18 @@ class Calculator extends Component {
     
     calculate = (event) => {
         event.preventDefault();
-        this.setState({
-            mathStr : this.state.mathStr,
-            result : calculate(this.state.mathStr)
-        })
+        var result = calculate(this.state.mathStr);
+        if (result.successFlag) {
+            this.setState({
+                result : result.value,
+                errorMessage : ''
+            })
+        } else {
+            this.setState({
+                result : '',
+                errorMessage : result.message
+            })
+        }
     }
     changeMathStr = (event) => {
         this.setState({
@@ -28,6 +37,16 @@ class Calculator extends Component {
     }
     
     render() {
+        let ErrorPanel = null;
+        if (this.state.errorMessage) {
+            ErrorPanel = (
+                <div id="errorPanelDiv">
+                    <ul>
+                        <li>{this.state.errorMessage} Please Try again.</li>
+                    </ul>
+                </div>
+            );
+        }
         return (
             <div className="maindiv">
                 <form id="calculatorForm">
@@ -35,7 +54,8 @@ class Calculator extends Component {
                         <h1 id="calcTitle">Calculator - VidMob Exercise</h1>
                     </header>
                     <div>
-                        <p className="calcDesrciption">Enter math string and click on Calculate button. Ex: "1 + 2", "(4-2)*3.5" etc </p>    
+                        <p className="calcDesrciption">Enter math string and click on Calculate button. Ex: "1 + 2", "(4-2)*3.5" etc </p>
+                        {ErrorPanel}
                         <div className="innerDiv">
                             <label id="mathStrLabel">Enter Problem : </label>
                             <textarea onChange={this.changeMathStr} value={this.state.mathStr} rows="5" cols="45"/>
--- src/CalculatorService.js
+++ src/CalculatorService.js
@@ -1,3 +1,37 @@
 export function calculate(input) {
-    return input + input
+    var acceptedFormat = /^[\d+*\-%/(). ]+$/g
+    if (!validateFormat(acceptedFormat, input)) {
+        return {'successFlag' : false, 'message': 'Invalid Input.'}
+    }
+    return evaluate(input.replaceAll(' ', ''))
+}
+
+function evaluate (textExp) {
+    var innerExpCheck = textExp.indexOf('(')
+    if (innerExpCheck === -1) {
+       var acceptedFormat = /^[+-]?\d+(.\d+)?\s?([+*%\-/]\s?[+-]?\d+(.\d+)?\s?)*$/g
+        if (!validateFormat(acceptedFormat, textExp)) {
+            return {'successFlag' : false, 'message': 'Syntax Error.'}
+        }
+        //To-Do implementation
+        return {'successFlag' : true, 'value': eval(textExp)}
+    } else {
+        var firstCloseExp = textExp.indexOf(')')
+        if (firstCloseExp === -1) {
+            return {'successFlag' : false, 'message': 'Syntax Error.'}
+        }
+        var priorOpenExp = textExp.substring(0, firstCloseExp).lastIndexOf('(')
+        var innerExp = textExp.substring(priorOpenExp+1, firstCloseExp)
+        var result = evaluate(innerExp);
+        if (result.successFlag) {
+            textExp = textExp.replaceAll('('+innerExp+')', result.value)
+            return evaluate(textExp)
+        } else {
+            return result;
+        }
+    }
+}
+
+function validateFormat (regex, testString) {
+    return regex.test(testString);
 }
